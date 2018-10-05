edb85115b6724248486e2a20885560bc719ceabe
Kelvin Ramirez <kelvinramirez@Administrators-iMac.local>
2018-10-05 11:36:43 -0400

    goin on

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -6,6 +6,7 @@
     <v-spacer></v-spacer>
     <v-toolbar-items class="hidden-sm-and-down">
       <v-btn flat><nuxt-link to="/">Inicio</nuxt-link></v-btn>
+       <v-btn flat><nuxt-link to="/post">Posts</nuxt-link></v-btn>
       <v-btn flat><nuxt-link to="/about">Nosotros</nuxt-link></v-btn>
       <v-btn flat> <nuxt-link to="/contacts">Contactos</nuxt-link> </v-btn>
     </v-toolbar-items>
--- pages/post/index.vue
+++ pages/post/index.vue
@@ -2,18 +2,20 @@
 <b-container>
     <h1>Post pages</h1>
 
-    <Admin_post_form/>
-
+    <!-- <Admin_post_form/> -->
+<Post_list/>
 </b-container>
     
 </template>
 
 <script>
-import Admin_post_form from "@/components/admin/Admin_post_form.vue";
+// import Admin_post_form from "@/components/admin/Admin_post_form.vue";
+import Post_list from "@/components/blog/Post_list.vue";
 export default {
   layout: "main_layout",
   components: {
-    Admin_post_form
+    // Admin_post_form
+    Post_list
   }
 };
 </script>
